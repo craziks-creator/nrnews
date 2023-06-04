79a1604473f0446d780a74612eda46347b7f27c0
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-06-04 17:47:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,13 +56,36 @@ ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
 #ie1.set_id_policy(ssc_id_policy)
 # News postman to manage sending affair
-np1 = NewsPostman(listURLs=[url2, ], sendList=[channel,channel2, ], db=db, tag=tag1)
+np1 = NewsPostman(listURLs=[url2, ], sendList=[channel,channel2, ], db=db, tag=tag2)
 np1.set_bot_token(bot_token)
 np1.set_extractor(ie1)
 np1.set_table_name(table_name2)
 np1.set_max_list_length(25)
 np1.set_max_table_rows(25 * 3, False)
 np1.poll()
+#-------------------------channel 3----------------------------------#
+url3 = "https://ssc.nic.in/Portal/Results"
+tag3 = "chsl result"
+table_name3 = "chsl"
+# Info extractor to process data format
+ie1 = InfoExtractor()
+# Select elements by CSS-based selector
+ie1.set_list_selector('#noticeschsl > div.noticeTxt a') #id_ul_li
+ie1.set_title_selector('span')  #id
+ie1.set_paragraph_selector('#noticeschsl > div.noticeTxt a[href]')
+ie1.set_time_selector('td')
+ie1.set_source_selector('span.sourceTemplate')
+ie1.max_post_length = 2000
+#ie1.set_id_policy(ssc_id_policy)
+# News postman to manage sending affair
+np1 = NewsPostman(listURLs=[url3, ], sendList=[channel,channel2, ], db=db, tag=tag3)
+np1.set_bot_token(bot_token)
+np1.set_extractor(ie1)
+np1.set_table_name(table_name3)
+np1.set_max_list_length(25)
+np1.set_max_table_rows(25 * 3, False)
+np1.poll()
+
 """
 #-------------------------channel 3----------------------------------#
 url2 = "https://www.scmp.com/rss/91/feed"
