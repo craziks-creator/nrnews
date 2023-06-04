e1566c0f51b109273662707827a3be98879222df
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-06-05 00:19:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ ie1.set_paragraph_selector('#noticesothers > div.noticeTxt a')
 """
 ie1.set_list_selector('#noticesothers > div.noticeTxt tr:nth-child(-n+4) a') #id_ul_li
 ie1.set_title_selector('#noticesothers > div.noticeTxt span ')  #id
-ie1.set_paragraph_selector('#noticesothers > div.noticeTxt tr:nth-child(-n+4) a[href])
+ie1.set_paragraph_selector('#noticesothers > div.noticeTxt tr:nth-child(-n+4) a[href]')
 ie1.set_time_selector('td')
 ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
