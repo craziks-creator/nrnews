4d3e704e5af1af75f14873ef89bf2c5d923fa04f
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-06-04 21:49:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -72,9 +72,9 @@ table_name4 = "others"
 # Info extractor to process data format
 ie1 = InfoExtractor()
 # Select elements by CSS-based selector
-ie1.set_list_selector('#noticesothers > div.noticeTxt span ') #id_ul_li
+ie1.set_list_selector('#noticesothers > div.noticeTxt') #id_ul_li
 ie1.set_title_selector('td > span')  #id
-ie1.set_paragraph_selector('#noticesothers > div.noticeTxt a[href]')
+ie1.set_paragraph_selector('#noticesothers > div.noticeTxt span')
 ie1.set_time_selector('td')
 ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
