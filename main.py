7b4e97e69588052c27cd538e71d9c033613a69b6
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-06-08 20:57:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ ie1 = InfoExtractor()
 #ie1.set_list_selector('div.eachNotification a') #id_ul_li
 ie1.set_list_selector('div.eachNotification a, div.scrollingNotifications_New>ul>li:nth-child(-n+3)>a')
 ie1.set_title_selector('#forScrollNews')  #id
-ie1.set_paragraph_selector('div.scrollingNotifications_New>ul>li a')
+ie1.set_paragraph_selector('a[href]')
 ie1.set_time_selector('span')
 ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
