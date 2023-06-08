ef728c8e07bdfa16a37f028c1ec134b1584319e3
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2023-06-08 12:16:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -145,10 +145,9 @@ table_name6 = "ssc"
 # Info extractor to process data format
 ie1 = InfoExtractor()
 # Select elements by CSS-based selector
-ie1.set_list_selector('div.scrollingNotifications_New>ul>li') #id_ul_li
+ie1.set_list_selector('div.scrollingNotifications_New>ul>li a') #id_ul_li
 ie1.set_title_selector('#forScrollNews')  #id
-ie1.set_paragraph_selector('div.scrollingNotifications_New>ul>li>a[href]')
-
+ie1.set_paragraph_selector('div.scrollingNotifications_New>ul>li a[href]')
 ie1.set_time_selector('span')
 ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
